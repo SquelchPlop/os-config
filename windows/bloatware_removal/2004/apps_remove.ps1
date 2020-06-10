95ec4d68cb20e0aea565ca39bf3b6ed8ca1d8f63
SquelchPlop <matthewhadley16@hotmail.com>
2020-06-10 23:15:06 +0100

    feat: initial commit

---
+++ windows/bloatware_removal/2004/apps_remove.ps1
@@ -0,0 +1,8 @@
+cd $env:temp
+
+Invoke-RestMethod -Method Get -URI https://raw.githubusercontent.com/Digressive/Remove-Win10-Apps/master/Remove-Win10-Apps.ps1 -OutFile Remove-Win10-Apps.ps1
+Invoke-RestMethod -Method Get -URI https://raw.githubusercontent.com/SquelchPlop/windows-config/master/bloatware_removal\2004\apps.txt -OutFile apps.txt
+./Remove-Win10-Apps.ps1 -List ./apps.txt -L ./
+
+Write-Host "Success"
+exit 0
---
+++ windows/printers/set_printers.ps1
@@ -0,0 +1,53 @@
+﻿#Only printers in this list can be installed.  Any printers outside this list will be removed.
+$AllowedPrinters = "PRINTER-01 Mono Laser", "PRINTER-02 Colour MFP", "Adobe PDF"
+
+#First printer in this hash table will be set as default.
+$Printers = @(
+    @{
+        Name = "PRINTER-01 Mono Laser";
+        HostAddress = "PRINTER-01";
+        DriverFile = "\\Files\Software\Repository\_Drivers\Brother Enhanced Generic PCL Driver\1.07\eng\BHPCL5E.INF";
+        DriverName="Brother PCL5e Driver";
+        Location="Peaslake Close"; 
+        Duplex=$True;
+        Color=$False;
+        PaperSize="A4";
+    }
+    @{
+        Name = "PRINTER-02 Colour MFP";
+        HostAddress = "PRINTER-02";
+        DriverFile = "\\Files\Software\Repository\_Drivers\HP PW477DW\38.7.1931\hpmaD5114_x64.inf";
+        DriverName="HP PageWide Pro 477dw MFP PCL-6";
+        Location="Peaslake Close"; 
+        Duplex=$True;
+        Color=$True;
+        PaperSize="A4";
+    }
+    
+)
+
+#Remove surplus printers.
+$InstalledPrinters = Get-Printer | select -ExpandProperty Name
+$InstalledPrinters | foreach {If(-Not ($AllowedPrinters -contains $_)){ Remove-Printer -Name $_}}
+
+#Install printers from the $Printers hash table, provided they're in the $AllowedPrinters array.
+$First=$True
+$Printers | ForEach {
+    If(($InstalledPrinters -contains $_.Name) -or ($AllowedPrinters -notcontains $_.Name)){} #If already installed or not allowed, skip
+    Else{
+        Invoke-Command {pnputil.exe /add-driver "$($_.DriverFile)"} | Out-Null #Add driver to repo
+        Add-PrinterDriver -Name $_.DriverName
+        Add-PrinterPort -Name "$($_.HostAddress) Port" -PrinterHostAddress $_.HostAddress
+        Add-Printer -Name $_.Name -PortName "$($_.HostAddress) Port" -DriverName $_.DriverName -Location $_.Location
+        If($_.Duplex){
+            Get-PrinterProperty    -PrinterName $_.Name -PropertyName "Config:DuplexUnit" -ComputerName $env:COMPUTERNAME -ErrorAction Ignore | Set-PrinterProperty -Value "Installed"
+            Set-PrintConfiguration -PrinterName $_.Name -DuplexingMode TwoSidedLongEdge 
+        }
+        Set-PrintConfiguration -PrinterName $_.Name -Color $_.Color -PaperSize $_.PaperSize
+    }
+    If($First){
+        $First=$False
+        $Default = Get-WmiObject -Query "Select * from win32_printer where name='$($_.Name)'"
+        $Default.SetDefaultPrinter() | Out-Null
+    }
+}
