ca55c7c3710eceabbcdb13c2472723ef1b4d024b
SquelchPlop <matthewhadley16@hotmail.com>
2020-06-16 11:45:52 +0100

    feat: cleanup apps removal, split out golden-images into separate repo

--- windows/bloatware_removal/apps_remove.ps1
+++ windows/bloatware_removal/apps_remove.ps1
@@ -1,8 +1,24 @@
+# Apps to remove
+$appsToRemove = "Microsoft.Microsoft3DViewer
+Microsoft.549981C3F5F10
+Microsoft.WindowsFeedbackHub
+Microsoft.ZuneVideo
+Microsoft.GetHelp
+Microsoft.ZuneMusic
+Microsoft.MicrosoftSolitaireCollection
+Microsoft.MixedReality.Portal
+Microsoft.MicrosoftOfficeHub
+Microsoft.Getstarted"
+
 Set-Location $env:temp
 
 # Remove UWP apps
 Invoke-RestMethod -Method Get -URI https://raw.githubusercontent.com/Digressive/Remove-Win10-Apps/master/Remove-Win10-Apps.ps1 -OutFile Remove-Win10-Apps.ps1
-Invoke-RestMethod -Method Get -URI https://raw.githubusercontent.com/SquelchPlop/os-config/master/windows/bloatware_removal/apps.txt -OutFile apps.txt
-./Remove-Win10-Apps.ps1 -List ./apps.txt -L ./
+
+# Apps to remove in txt file
+$appsToRemove | Out-File appsToRemove.txt
+
+# Perform removal
+./Remove-Win10-Apps.ps1 -List ./appsToRemove.txt -L ./
 
 Write-Output "Removed apps"
--- windows/golden_image/SetupComplete.cmd
+++
@@ -1,9 +0,0 @@
-cd %TEMP%
-
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/SquelchPlop/os-config/master/windows/bloatware_removal/apps_remove.ps1'))"
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/SquelchPlop/os-config/master/windows/bloatware_removal/features_remove.ps1'))"
-
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; Invoke-RestMethod -Method Get -URI https://raw.githubusercontent.com/TheJumpCloud/support/master/scripts/windows/InstallWindowsAgent.ps1 -OutFile InstallWindowsAgent.ps1"
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "./InstallWindowsAgent.ps1 -JumpCloudConnectKey 'xxx'"
-
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/SquelchPlop/os-config/master/windows/windows_security_baselines/apply_wsb_1909_win10_nondomain.ps1'))"
