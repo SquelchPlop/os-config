00371134bb767f965041d2c12b81c0d1571676a5
SquelchPlop <matthewhadley16@hotmail.com>
2020-06-16 11:35:12 +0100

    feat: remove hardcode version

--- windows/bloatware_removal/apps_remove.ps1
+++ windows/bloatware_removal/apps_remove.ps1
@@ -2,7 +2,7 @@ Set-Location $env:temp
 
 # Remove UWP apps
 Invoke-RestMethod -Method Get -URI https://raw.githubusercontent.com/Digressive/Remove-Win10-Apps/master/Remove-Win10-Apps.ps1 -OutFile Remove-Win10-Apps.ps1
-Invoke-RestMethod -Method Get -URI https://raw.githubusercontent.com/SquelchPlop/os-config/master/windows/bloatware_removal/2004/apps.txt -OutFile apps.txt
+Invoke-RestMethod -Method Get -URI https://raw.githubusercontent.com/SquelchPlop/os-config/master/windows/bloatware_removal/apps.txt -OutFile apps.txt
 ./Remove-Win10-Apps.ps1 -List ./apps.txt -L ./
 
 Write-Output "Removed apps"
--- windows/golden_image/SetupComplete.cmd
+++ windows/golden_image/SetupComplete.cmd
@@ -1,7 +1,7 @@
 cd %TEMP%
 
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/SquelchPlop/os-config/master/windows/bloatware_removal/2004/apps_remove.ps1'))"
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/SquelchPlop/os-config/master/windows/bloatware_removal/2004/features_remove.ps1'))"
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/SquelchPlop/os-config/master/windows/bloatware_removal/apps_remove.ps1'))"
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/SquelchPlop/os-config/master/windows/bloatware_removal/features_remove.ps1'))"
 
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; Invoke-RestMethod -Method Get -URI https://raw.githubusercontent.com/TheJumpCloud/support/master/scripts/windows/InstallWindowsAgent.ps1 -OutFile InstallWindowsAgent.ps1"
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "./InstallWindowsAgent.ps1 -JumpCloudConnectKey 'xxx'"
